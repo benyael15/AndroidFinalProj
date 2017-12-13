3857728a589fa39ae456a7994184ebbb18246cae
ben yael <benyael15@gmail.com>
2017-12-13 18:20:54 +0200

    Initial commit . empty rpoject + FireBase configuration

---
+++ app/src/main/java/com/benya/pickasit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.benya.pickasit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
